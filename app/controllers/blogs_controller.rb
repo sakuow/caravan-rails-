a7892e7af9843061388a96cd4dc5e59a259ce5b7
sakuow <yusaku1335@gmail.com>
2021-09-29 09:58:19 +0000

    4 commit

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,7 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
+    
   end
 
   def show
@@ -9,5 +11,17 @@ class BlogsController < ApplicationController
   end
 
   def new
+    @blog = Blog.new
+  end
+
+  def create
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :category, :body)
   end
 end
